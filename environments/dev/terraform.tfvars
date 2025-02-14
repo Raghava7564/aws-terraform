a337ad422827ae8d11476456bc5cd610f74b1bba
Raghava7564 <raghavalakkamraju@gmail.com>
2025-02-14 06:32:42 +0000

    push the code

---
+++ environments/dev/terraform.tfvars
@@ -0,0 +1,3 @@
+ami            = "ami-12345678"  # Replace with a valid AMI ID
+instance_type  = "t2.micro"
+instance_name  = "DevInstance"
---
+++ modules/ec2/variables.tf
@@ -0,0 +1,15 @@
+variable "ami" {
+  description = "Amazon Machine Image ID"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "instance_name" {
+  description = "Name tag for the instance"
+  type        = string
+}
