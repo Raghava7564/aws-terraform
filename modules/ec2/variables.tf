9f81102f2254692d7dbd2826876796dbc93c2b67
Raghava7564 <raghavalakkamraju@gmail.com>
2025-02-14 14:37:58 +0530

    Update variables.tf

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -2,6 +2,10 @@ variable "ami" {
   description = "The AMI ID for the EC2 instance"
   type        = string
 }
+variable "instance_name" {
+  description = "The name of the EC2 instance"
+  type        = string
+}
 
 variable "instance_type" {
   description = "The instance type"
