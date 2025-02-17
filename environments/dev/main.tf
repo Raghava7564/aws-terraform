ee9e427cbb49fb792c58627689edb45379470be4
Raghava7564 <raghavalakkamraju@gmail.com>
2025-02-17 15:24:36 +0530

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,7 +1,26 @@
-module "ec2" {
-  source         = "git::https://github.com/Raghava7564/aws-terraform.git//modules/ec2"
+module "s3" {
+  source      = "git::https://github.com/Raghava7564/terraform-aws-modules.git//aws-modules/s3"
+  bucket_name = var.bucket_name
+  acl         = var.acl
+}
 
-  instance_name  = "my-ec2-instance"   # Provide a unique instance name
-  ami           = "ami-01e3c4a339a264cc9"  # Corrected from ami_id
-  instance_type = "t2.micro"  
+module "ec2" {
+  source         = "git::https://github.com/Raghava7564/terraform-aws-modules.git//aws-modules/ec2"
+  ami           = var.ami
+  instance_type = var.instance_type
+  instance_name = var.instance_name
+}
+
+module "ecs" {
+  source          = "git::https://github.com/Raghava7564/terraform-aws-modules.git//aws-modules/ecs"
+  ecs_cluster_name = var.ecs_cluster_name
+}
+
+module "lambda" {
+  source        = "git::https://github.com/Raghava7564/terraform-aws-modules.git//aws-modules/lambda"
+  function_name = var.function_name
+  role_arn      = var.role_arn
+  handler       = var.handler
+  runtime       = var.runtime
+  filename      = var.filename
 }
