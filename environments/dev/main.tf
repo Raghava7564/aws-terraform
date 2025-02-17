50a067347ca73bfae05be3e4c2cff0eca3abbff0
Raghava7564 <raghavalakkamraju@gmail.com>
2025-02-17 14:30:37 +0530

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -2,6 +2,6 @@ module "ec2" {
   source         = "git::https://github.com/Raghava7564/aws-terraform.git//modules/ec2"
 
   instance_name  = "my-ec2-instance"   # Provide a unique instance name
-  ami           = "ami-0c55b159cbfafe1f0"  # Corrected from ami_id
+  ami           = "ami-01e3c4a339a264cc9"  # Corrected from ami_id
   instance_type = "t2.micro"  
 }
