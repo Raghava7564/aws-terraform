24727f24437362822640fb74101876d3f28d4e8e
Raghava7564 <raghavalakkamraju@gmail.com>
2025-02-17 15:34:05 +0530

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,23 +1,23 @@
 module "s3" {
-  source      = "git::https://github.com/Raghava7564/terraform-aws-modules.git//aws-modules/s3"
+  source      = "git::https://github.com/Raghava7564/aws-terraform.git//modules/s3"
   bucket_name = var.bucket_name
   acl         = var.acl
 }
 
 module "ec2" {
-  source         = "git::https://github.com/Raghava7564/terraform-aws-modules.git//aws-modules/ec2"
+  source         = "git::https://github.com/Raghava7564/aws-terraform.git//modules/ec2"
   ami           = var.ami
   instance_type = var.instance_type
   instance_name = var.instance_name
 }
 
 module "ecs" {
-  source          = "git::https://github.com/Raghava7564/terraform-aws-modules.git//aws-modules/ecs"
+  source          = "git::https://github.com/Raghava7564/aws-terraform.git//modules/ecs"
   ecs_cluster_name = var.ecs_cluster_name
 }
 
 module "lambda" {
-  source        = "git::https://github.com/Raghava7564/terraform-aws-modules.git//aws-modules/lambda"
+  source        = "git::https://github.com/Raghava7564/aws-terraform.git//modules/lambda"
   function_name = var.function_name
   role_arn      = var.role_arn
   handler       = var.handler
